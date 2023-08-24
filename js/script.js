6710617732dc16b755eebaeeb54f02f28cfb1832
aalexebr <alex.ebr@icloud.com>
2023-08-24 15:36:01 +0200

    ADD simple print

--- js/script.js
+++ js/script.js
@@ -3,13 +3,15 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        axiosData:[]
       }
     },
     created(){
         axios.get('http://localhost/php-dischi-json/api.php',{})
             .then(result =>{
                 console.log(result.data);
+                this.axiosData = result.data
             });
     }
   }).mount('#app')
