482e2f04806b266c722cdf4ac228ee9b8b0ffa4e
aalexebr <alex.ebr@icloud.com>
2023-08-24 15:26:58 +0200

    ADD axios function to get api

--- js/script.js
+++ js/script.js
@@ -5,5 +5,11 @@ const { createApp } = Vue
       return {
         message: 'Hello Vue!'
       }
+    },
+    created(){
+        axios.get('http://localhost/php-dischi-json/api.php',{})
+            .then(result =>{
+                console.log(result.data);
+            });
     }
   }).mount('#app')
