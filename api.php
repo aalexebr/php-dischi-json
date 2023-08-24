d6f4a54bb51160503ac82517babd90d53f5d3d70
aalexebr <alex.ebr@icloud.com>
2023-08-24 15:23:05 +0200

    ADD json encode api_php file

--- api.php
+++ api.php
@@ -1,5 +1,7 @@
 <?php
     $data_string = file_get_contents('./database/data.json');
-    var_dump($data_string);
+    // var_dump($data_string);
     $data_to_php = json_decode($data_string,true);
-    var_dump($data_to_php);
+    // var_dump($data_to_php);
+    header('Content-Type: application/json');
+    echo json_encode($data_to_php);
